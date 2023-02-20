86bce35819814e1ef8ff97645db486b2e32dd700
Benoit Leboucher <ben.lebou@gmail.com>
2023-02-20 15:12:31 -0500

    Created bascis seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,13 @@ Flat.destroy_all
 
 puts "Creating flats..."
 
-flat1.create!(name: 'Appart in Paris', description: 'Amazing', address: 'Paris 11', price: 100)
-flat1.create!(name: 'Appart in London', description: 'Amazing', address: 'London 11', price: 100)
-flat1.create!(name: 'Appart in Montreal', description: 'Amazing', address: 'Montreal 11', price: 100)
-flat1.create!(name: 'Appart in New York', description: 'Amazing', address: 'New York 11', price: 100)
+flat1 = Flat.create(name: 'Appart in Paris', description: 'Amazing', address: 'Paris 11', price: 100)
+flat2 = Flat.create(name: 'Appart in London', description: 'Amazing', address: 'London 11', price: 100)
+flat3 = Flat.create(name: 'Appart in Montreal', description: 'Amazing', address: 'Montreal 11', price: 100)
+flat4 = Flat.create(name: 'Appart in New York', description: 'Amazing', address: 'New York 11', price: 100)
+
+user1 = User.create(username: "ben", )
+
+flat1.user_id =
 
 puts "Finished!"
