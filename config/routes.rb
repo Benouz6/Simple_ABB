59de5ec5b2fe698a094484038e8cd41f2ee48782
Benoit Leboucher <ben.lebou@gmail.com>
2023-02-20 18:08:06 -0500

    Create the seeds

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "flats#index"
   get 'flats', to: 'flats#index'
+  get "flats/:id", to: "flats#show", as: :flat
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ require 'faker'
 
 puts "Cleaning database..."
 Flat.destroy_all
+User.destroy_all
 
 puts "Creating flats..."
 
@@ -10,8 +11,25 @@ flat2 = Flat.create(name: 'Appart in London', description: 'Amazing', address: '
 flat3 = Flat.create(name: 'Appart in Montreal', description: 'Amazing', address: 'Montreal 11', price: 100)
 flat4 = Flat.create(name: 'Appart in New York', description: 'Amazing', address: 'New York 11', price: 100)
 
-user1 = User.create(username: "ben")
+puts 'Created flats'
+puts 'create user'
+user1 = User.create(username: "ben", email: "b@b.b", password: '123456')
+user2 = User.create(username: "cris", email: "c@c.c", password: '123456')
 
-flat1.user_id =
+user1.save
+user2.save
+
+
+flat1.user_id = user1.id
+flat1.save!
+
+flat2.user_id = user1.id
+flat2.save!
+
+flat3.user_id = user2.id
+flat3.save!
+
+flat4.user_id = user2.id
+flat4.save!
 
 puts "Finished!"
