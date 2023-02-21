1aaab737813e41264be10d4f45bc3e00919c883f
Benoit Leboucher <ben.lebou@gmail.com>
2023-02-21 10:26:30 -0500

    Adding a flat done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "flats#index"
-  get 'flats', to: 'flats#index'
-  get "flats/:id", to: "flats#show", as: :flat
+  # get 'flats', to: 'flats#index'
+  # get "flats/new", to: "flats#new"
+  # get "flats/:id", to: "flats#show", as: :flat
+  # post "flats", to: "flats#create"
+  resources :flats
+
 end
